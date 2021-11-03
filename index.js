622adaf31fb54f523bb4a4dda777dfdd4740b63f
Ricardo Robert <46797052+Ricardoalmeida54@users.noreply.github.com>
2021-11-03 02:43:57 -0300

    BACK END => CRIANDO REDUX/SAGA

--- index.js
+++ index.js
@@ -5,10 +5,16 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+import { Provider } from 'react-redux';
+import store from './src/store';
 import Car from './src/pages/Steps/car'
 import Login from './src/pages/Login'
 import Type from './src/pages/Steps/type'
 import Payment from './src/pages/Steps/payment'
 import Ride from './src/pages/RIde'
 import Home from './src/pages/Home'
-AppRegistry.registerComponent(appName, () => Home);
+AppRegistry.registerComponent(appName, () => 
+<Provider store={store}>
+<Home />
+</Provider>);
---
+++ src/store/modules/app/reducer.js
@@ -0,0 +1,23 @@
+import types from './types';
+import produce from 'immer';
+const INITIAL_STATE = {
+    user: {
+        fbId: null,
+        nome: null,
+        email: null,
+        tipo: 'M',
+        accesssToken: null,
+    },
+};
+
+function app(state = INITIAL_STATE, action) {
+switch(action.types) {
+case types.UPDATE_USER: {
+    return produce(state, (draft) => {
+        draft.user = { ...state.user, ...action.user};
+    });
+    }
+}
+}
+
+export default app;
